82f11269781e9364fe523564fa46794ed47e6a01
juru-juliette <kangabejuliette@gmail.com>
2019-09-27 09:27:08 +0200

    configuring my app

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+class Config:
+
+
+    SECRET_KEY = 'juru1'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:  @localhost/myapp'
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
