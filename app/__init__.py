7c0eff1992e323589580eabe5c4370bd0d9edabe
juru-juliette <kangabejuliette@gmail.com>
2019-09-28 16:21:31 +0200

    adding style

--- app/__init__.py
+++ app/__init__.py
@@ -35,7 +35,7 @@ def create_app(config_name):
     # configure UploadSet
     configure_uploads(app,photos)
     # setting config
-    from .request import configure_request
+    from .requests import configure_request
     configure_request(app)
   
     return app
